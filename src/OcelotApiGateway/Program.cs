e067dd7be187dbfa8049a3097e63ed18f8b2d491
FrederikBA <frederikbandersen98@gmail.com>
2023-12-25 10:34:49 +0100

    add ocelot configuration json file

--- src/OcelotApiGateway/Program.cs
+++ src/OcelotApiGateway/Program.cs
@@ -1,6 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+
+
+var app = builder.Build();
+await app.UseOcelot();
 
 app.Run();
